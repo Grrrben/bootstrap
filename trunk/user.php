4b3bddee2c5ed6859c97e741ae0c2cf352bc4729
Grrrben <grrrben@Ubuntu.(none)>
2014-09-11 20:51:58 +0200

    user.php

---
+++ trunk/user.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * PHP eerst
+ * PHP wordt op de server uitgevoerd (server side)
+ * HTML is wat de browser laat zien (client side)
+ */
+
+// Directory /var/www/bootstrap/trunk
+
+
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/favicon.ico">
+
+    <title>Signin Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <p>container</p>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="/js/ie-hack.js"></script>
+  </body>
+</html>
