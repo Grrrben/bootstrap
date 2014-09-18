5fbc757331785525b8c1a13361d944ec6f2c2485
danny <danny@PC-JPYC45J.adnl.local>
2014-09-18 19:20:45 +0200

    Style toegevoegd aan de formulieren

--- trunk/index.php
+++ trunk/index.php
@@ -30,7 +30,8 @@ if (!empty($_POST)) {
 }
 
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -40,13 +41,13 @@ if (!empty($_POST)) {
     <meta name="author" content="">
     <link rel="icon" href="/favicon.ico">
 
-    <title>Signin Template for Bootstrap</title>
+    <title>Test server</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="/css/custom.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -73,6 +74,6 @@ if (!empty($_POST)) {
 
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="/js/ie-hack.js"></script>
+    <script src="js/ie-hack.js"></script>
   </body>
 </html>
