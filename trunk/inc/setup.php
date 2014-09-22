a2389f6c7c277ebd5e3d12c46595ddda5c5dbae7
Grrrben <grrrben@Ubuntu.(none)>
2014-09-22 10:53:31 +0200

    setup, comment

--- trunk/inc/setup.php
+++ trunk/inc/setup.php
@@ -7,5 +7,6 @@ require_once ('db_settings.php');
 // @url http://php.net/manual/en/book.mysqli.php
 $mysqli = new mysqli("localhost", $username, $password, $dbname);
 if ($mysqli->connect_errno) {
+	echo 'Er is een database error. De connectie staat in /trunk/inc/setup.php. Je kunt de connectie even uitzetten door de regelsa te commenten met //. <br><br>';
 	die ("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);	
 }
